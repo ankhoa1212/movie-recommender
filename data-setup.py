50fe6c9f8f668255f18ecfa2d2f7367c4c271f7b
Austin <ankhoa1212@gmail.com>
2024-08-14 23:43:02 -0700

    add data for ratings

--- data-setup.py
+++ data-setup.py
@@ -10,41 +10,42 @@ from bs4 import BeautifulSoup
 DATASET_URL = 'https://datasets.imdbws.com/'
 
 # target dataset link to download
-TARGET = 'title.basics.tsv.gz'
+TARGETS = ['title.basics.tsv.gz', 'title.ratings.tsv.gz']
 
-if not os.path.isfile(TARGET[:TARGET.rfind('.')]):
-    exit()
+for target in TARGETS:
+    if os.path.isfile(target[:target.rfind('.')]):
+        continue
 
-# get html from webpage with dataset files
-with urllib.request.urlopen(DATASET_URL) as f:
-    html = f.read()
+    # get html from webpage with dataset files
+    with urllib.request.urlopen(DATASET_URL) as f:
+        html = f.read()
 
-# create BeautifulSoup object for parsing
-soup = BeautifulSoup(html,features='html.parser')
+    # create BeautifulSoup object for parsing
+    soup = BeautifulSoup(html,features='html.parser')
 
-# find target file to download
-download_link = ''
-for link in soup.find_all('a'):
-    link_check = link.get('href')
-    if TARGET in link_check:
-        print(f'downloading file: {TARGET}')
-        break
+    # find target file to download
+    download_link = ''
+    for link in soup.find_all('a'):
+        link_check = link.get('href')
+        if target in link_check:
+            print(f'downloading file: {target}')
+            break
 
-urllib.request.urlretrieve(DATASET_URL + TARGET, TARGET)
-print('unzipping...')
-unzipped_file = gzip.open(TARGET)
-df = pd.read_csv(TARGET, sep='\t', header=0)
-df['endYear'] = df['endYear'].replace(['\\N'],None)
-df['runtimeMinutes'] = df['runtimeMinutes'].replace(['\\N'],None)
-df['isAdult'] = df['isAdult'].replace(['0'],False)
-df['isAdult'] = df['isAdult'].replace(['1'],True)
-print(f'saving {TARGET} as file...')
-# save tsv file
-df.to_csv(TARGET[:TARGET.rfind('.')], sep='\t', index=False)
-# TODO remove this section when testing is complete
-df.head(100).to_csv(TARGET[:TARGET.rfind('.')] + "_100", sep='\t', index=False)
-df.head(1000).to_csv(TARGET[:TARGET.rfind('.')] + "_1000", sep='\t', index=False)
-df.head(10000).to_csv(TARGET[:TARGET.rfind('.')] + "_10000", sep='\t', index=False)
-# remove downloaded file
-os.remove(TARGET)
-print(df)
+    urllib.request.urlretrieve(DATASET_URL + target, target)
+    print('unzipping...')
+    unzipped_file = gzip.open(target)
+    df = pd.read_csv(target, sep='\t', header=0)
+    df = df.replace('\\N', None)
+    if 'isAdult' in df.columns:
+        df['isAdult'] = df['isAdult'].replace(['0'],False)
+        df['isAdult'] = df['isAdult'].replace(['1'],True)
+    print(f'saving {target} as file...')
+    # save tsv file
+    df.to_csv(target[:target.rfind('.')], sep='\t', index=False)
+    # TODO remove this section when testing is complete
+    df.head(100).to_csv(target[:target.find('.tsv')] + "_100.tsv", sep='\t', index=False)
+    df.head(1000).to_csv(target[:target.find('.tsv')] + "_1000.tsv", sep='\t', index=False)
+    df.head(10000).to_csv(target[:target.find('.tsv')] + "_10000.tsv", sep='\t', index=False)
+    # remove downloaded file
+    os.remove(target)
+    print(df)
