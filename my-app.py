4eaf98e9873f97851fe9c03450caeb3595c0a552
Austin <ankhoa1212@gmail.com>
2024-08-14 19:33:57 -0700

    add template for using data in local directory and generate random recommendations

--- my-app.py
+++ my-app.py
@@ -1,4 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-st.write("""Hello *World*""")
+# TODO use entire dataset once testing is complete
+TARGET = 'title.basics.tsv_10000'
+num_recommendations = 3
+
+watch_list = ['tt0010034']
+recommendations = []
+
+st.title("Movie Recommendation System")
+
+st.write("*uses data from imdb non-commercial database*")
+df = pd.read_csv(TARGET, sep='\t', header=0, low_memory=False)
+st.dataframe(df)
+if st.button('Get Random Recommendations'):
+    # TODO make recommendations using k-means clustering
+    # randomize recommendations for now
+    recommendations = df.sample(n=num_recommendations)['tconst']
+    for r in recommendations:
+        rec = df['primaryTitle'].loc[df['tconst'] == r]
+        rec_str = rec.to_string(index=False)
+        rec_str
